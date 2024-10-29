e03bdc5c5d13c214e34bf60007b1ed9ce9cef4af
ramyus889 <ramyus889.com>
2024-10-29 20:12:13 +0300

    add

--- src/component/UseHeader.vue
+++ src/component/UseHeader.vue
@@ -1,6 +1,6 @@
 <script setup></script>
 <template>
-  <div class="flex gap-5 py-5 px-5">
+  <div class="flex gap-5 py-5 px-5 border-b border-gray-500">
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/RestApi">RestApi</RouterLink>
     <RouterLink to="/UserList">UserList</RouterLink>
--- src/component/UseUserList.vue
+++ src/component/UseUserList.vue
@@ -1,19 +1,27 @@
 <script setup>
-import axios from 'axios';
-import { onMounted, ref } from 'vue';
+import Button from 'primevue/button';
+import { onMounted } from 'vue';
+import UseButton from './UseButton.vue';
+import useHandlers from './useHandlers.js';
 
-const user = ref([]);
-
-const url = 'https://jsonplaceholder.typicode.com/users';
-
-const fetchUsers = async () => {
-  try {
-    const { data: res } = await axios.get(url);
-    user.value = res;
-  } catch (error) {
-    console.error(error);
-  }
-};
+const {
+  show,
+  user,
+  userSizes,
+  userRotate,
+  userBorder,
+  userOpacity,
+  fetchUsers,
+  handleClickPlus,
+  handleClickMinus,
+  handleClickRotatePlus,
+  handleClickRotateMinus,
+  handleClickBorderPlus,
+  handleClickBorderMinus,
+  handleClickOpacityMinus,
+  handleClickOpacityPlus,
+  handleClickShow
+} = useHandlers();
 
 onMounted(() => {
   fetchUsers();
@@ -21,12 +29,45 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex flex-col gap-10 mt-10 ms-10 max-w-[1300px]">
+  <div class="flex flex-col gap-10 mt-10 ms-10 max-w-[1400px]">
     <div class="my-5">User List</div>
     <div class="grid grid-cols-2 gap-5" v-auto-animate>
       <div v-for="item in user" :key="item.id">
-        <div class="UserCard w-full">
-          <span>{{ item.username }}</span>
+        <div class="UserCard w-full items-center justify-between">
+          <span
+            :style="{
+              fontSize: (userSizes[item.id] || 16) + 'px',
+              transform: 'rotate(' + (userRotate[item.id] || 0) + 'deg)',
+              border: (userBorder[item.id] || 1) + 'px solid white',
+              opacity: userOpacity[item.id] || 1
+            }"
+            class="transition-all px-3 py-1 rounded-xl"
+            >{{ item.username }}</span
+          >
+          <div class="flex flex-col items-center gap-3">
+            <div class="">
+              <Button
+                :severity="show[item.id] ? 'danger' : 'default'"
+                :label="show[item.id] ? 'Close' : 'Edit'"
+                @click="handleClickShow(item.id)"
+              />
+            </div>
+            <div v-if="show[item.id]" class="flex flex-col items-center gap-3">
+              <UseButton
+                :item="item"
+                :handlers="{
+                  handleClickPlus: handleClickPlus,
+                  handleClickMinus: handleClickMinus,
+                  handleClickRotatePlus: handleClickRotatePlus,
+                  handleClickRotateMinus: handleClickRotateMinus,
+                  handleClickBorderPlus: handleClickBorderPlus,
+                  handleClickBorderMinus: handleClickBorderMinus,
+                  handleClickOpacityMinus: handleClickOpacityMinus,
+                  handleClickOpacityPlus: handleClickOpacityPlus
+                }"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
