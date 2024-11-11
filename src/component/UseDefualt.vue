a75fc6aefb825073e088468740a8a4a066b33ada
ramyus889 <ramyus889.com>
2024-11-11 17:33:20 +0300

    add

--- src/component/UseDefualt.vue
+++ src/component/UseDefualt.vue
@@ -1,11 +1,66 @@
-<script setup></script>
+<script setup>
+let currentSpin = 0;
+
+const spinId = () => {
+  return ++currentSpin;
+};
+
+const items = [
+  {
+    id: spinId(),
+    delay: 100
+  },
+  {
+    id: spinId(),
+    delay: 150
+  },
+  {
+    id: spinId(),
+    delay: 200
+  },
+  {
+    id: spinId(),
+    delay: 300
+  },
+  {
+    id: spinId(),
+    delay: 500
+  },
+  {
+    id: spinId(),
+    delay: 700
+  },
+  {
+    id: spinId(),
+    delay: 1000
+  },
+  {
+    id: spinId(),
+    delay: 1500
+  },
+  {
+    id: spinId(),
+    delay: 900
+  },
+  {
+    id: spinId(),
+    delay: 1000
+  },
+  {
+    id: spinId(),
+    delay: 1500
+  }
+];
+</script>
 
 <template>
-  <div class="flex flex-col gap-4">
-    <div class="text-center UserBorder motion-preset-flomoji-[🧨]">Motion</div>
-    <div class="text-center UserBorder motion-preset-blink motion-preset-wobble">Motion</div>
-    <div class="text-center UserBorder motion-preset-confetti">Motion</div>
-    <div class="text-center UserBorder motion-preset-pulse">Motion</div>
-    <div class="text-center UserBorder motion-preset-float">Motion</div>
+  <div class="grid grid-cols-3 gap-20 mb-20">
+    <div class="text-center UserBorder motion-preset-spin motion-duration-100"></div>
+    <div v-for="item in items" :key="item.id" class="">
+      <div
+        :class="`motion-delay-${item.delay}`"
+        class="text-center UserBorder motion-blur-loop"
+      ></div>
+    </div>
   </div>
 </template>
