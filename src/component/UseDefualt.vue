9fdc627ea078b814db169f29188789e5b6430482
ramyus889 <ramyus889.com>
2024-11-13 17:05:10 +0300

    add

--- src/component/UseDefualt.vue
+++ src/component/UseDefualt.vue
@@ -1,66 +1,31 @@
-<script setup>
-let currentSpin = 0;
-
-const spinId = () => {
-  return ++currentSpin;
-};
-
-const items = [
-  {
-    id: spinId(),
-    delay: 100
-  },
-  {
-    id: spinId(),
-    delay: 150
-  },
-  {
-    id: spinId(),
-    delay: 200
-  },
-  {
-    id: spinId(),
-    delay: 300
-  },
-  {
-    id: spinId(),
-    delay: 500
-  },
-  {
-    id: spinId(),
-    delay: 700
-  },
-  {
-    id: spinId(),
-    delay: 1000
-  },
-  {
-    id: spinId(),
-    delay: 1500
-  },
-  {
-    id: spinId(),
-    delay: 900
-  },
-  {
-    id: spinId(),
-    delay: 1000
-  },
-  {
-    id: spinId(),
-    delay: 1500
-  }
-];
-</script>
-
 <template>
-  <div class="grid grid-cols-3 gap-20 mb-20">
-    <div class="text-center UserBorder motion-preset-spin motion-duration-100"></div>
-    <div v-for="item in items" :key="item.id" class="">
-      <div
-        :class="`motion-delay-${item.delay}`"
-        class="text-center UserBorder motion-blur-loop"
-      ></div>
+  <div class="w-[600px] bg-slate-600 shadow-lg rounded-lg p-6">
+    <div class="flex items-center gap-4">
+      <img
+        src="/img/Car/Car8.webp"
+        alt="Office"
+        class="w-[200px] h-[150px] object-cover rounded-md"
+      />
+      <div class="flex flex-col justify-between flex-1">
+        <p class="text-lg text-neutral-950">
+          This is some placeholder text for the right side of the image. It goes here and describes
+          the content.
+        </p>
+      </div>
+    </div>
+    <div class="w-full my-4 border-b-2 border-neutral-900"></div>
+    <div class="flex items-center justify-between">
+      <div class="flex text-neutral-950">
+        <p>
+          This is some placeholder text for the right side of the image. It goes here and describes
+          the content.
+        </p>
+      </div>
+      <img
+        src="/img/Car/Car9.webp"
+        alt="Office"
+        class="w-[200px] h-[150px] object-cover rounded-md"
+      />
     </div>
   </div>
 </template>
--- src/component/UseHeader.vue
+++ src/component/UseHeader.vue
@@ -1,16 +1,68 @@
 <script setup>
+import { useRoute } from 'vue-router';
 import { routeUrl } from '../router/routes.js';
+
+const useClass = 'transition-all hover:font-bold';
+const route = useRoute();
+
+function isActive(path) {
+  return route.path === path ? 'text-red-500 font-bold' : '';
+}
+
+const items = [
+  {
+    id: 1,
+    name: 'Home',
+    pathItems: `${routeUrl[0].path}`
+  },
+  {
+    id: 2,
+    name: 'RestApi',
+    pathItems: `${routeUrl[1].path}`
+  },
+  {
+    id: 3,
+    name: 'UserList',
+    pathItems: `${routeUrl[2].path}`
+  },
+  {
+    id: 4,
+    name: 'SearchList',
+    pathItems: `${routeUrl[3].path}`
+  },
+  {
+    id: 5,
+    name: 'DeleteUser',
+    pathItems: `${routeUrl[4].path}`
+  },
+  {
+    id: 6,
+    name: 'PushList',
+    pathItems: `${routeUrl[5].path}`
+  },
+  {
+    id: 7,
+    name: 'EditUser',
+    pathItems: `${routeUrl[6].path}`
+  },
+  {
+    id: 8,
+    name: 'GSAP',
+    pathItems: `${routeUrl[7].path}`
+  }
+];
 </script>
 
 <template>
-  <div class="flex gap-5 px-5 py-5 border-b border-gray-500">
-    <RouterLink :to="`${routeUrl[0].path}`">Home</RouterLink>
-    <RouterLink :to="`${routeUrl[1].path}`">RestApi</RouterLink>
-    <RouterLink :to="`${routeUrl[2].path}`">UserList</RouterLink>
-    <RouterLink :to="`${routeUrl[3].path}`">SearchList</RouterLink>
-    <RouterLink :to="`${routeUrl[4].path}`">DeleteUser</RouterLink>
-    <RouterLink :to="`${routeUrl[5].path}`">PushList</RouterLink>
-    <RouterLink :to="`${routeUrl[6].path}`">EditUser</RouterLink>
-    <RouterLink :to="`${routeUrl[7].path}`">GSAP</RouterLink>
+  <div
+    class="max-w-[700px] bg-gradient-to-r from-[#4b4b4c] to-[#3e0b35] rounded-full shadow-lg mx-auto mt-5 flex place-content-center items-center px-4 py-3"
+  >
+    <div class="flex items-center gap-5 px-5 py-3 place-content-center">
+      <div v-for="item in items" :key="item.id" class="">
+        <RouterLink :to="item.pathItems" :class="[useClass, isActive(item.pathItems)]">{{
+          item.name
+        }}</RouterLink>
+      </div>
+    </div>
   </div>
 </template>
